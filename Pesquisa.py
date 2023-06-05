9e20cbaed35d3f0ed99b5a5c899c177596725c11
Julianapvh <julianacore2@gmail.com>
2023-06-05 10:23:13 -0400

    Alteração no tratamento de erros

--- Pesquisa.py
+++ Pesquisa.py
@@ -15,69 +15,76 @@ from datetime import datetime
 
 class Pesquisa:
     def __init__(self):
-        self.dados_respostas = []
-    
-    # Função criada para coletar dados e respostas da pesquisa
+        self.dados_respostas = []  # Inicializa a lista que irá armazenar as respostas dos participantes
+
     def coletar_respostas(self):
-
-        # Utilizando while para dar segmento na pesquisa até que seja encerrada via o comando solicitado
         while True:
-            print('-'*100)
-            print('\033[33mOlá! Esta é uma pesquisa sobre equilíbrio entre vida pessoal, trabalho e saúde mental.')
-            idade = input("\033[0;0mInforme sua idade (ou '00' para sair): ")
-            print('-'*100)
-            if idade == '00':
-                break
-            
-            print('-'*100)
-            genero = input("Informe seu gênero\n1 - Feminino\n2 - Masculino\n3 - Transgênero\n4 - Outro\n ")
-            print('-'*100)
-            # Dicionário para guardar os valores correspondetes à pesquisa de gênero, utilizando .get para acessá-lo na entrada 'genero'
-            genero_opcao = {
-                '1': 'Feminino',
-                '2': 'Masculino',
-                '3': 'Transgênero',
-                '4': 'Outro'}.get(genero)
-            
-            # Entradas para coletar as respostas da pesquisa
-            resposta1 = input("A sua empresa oferece suporte adequado à saúde mental dos funcionários?\n1-Sim\n2-Não\n3-Não sei responder\n ")
-            print('-'*100)
-            resposta2 = input("Você ja passou por situações de sobrecarga no seu trabalho?\n1-Sim\n2-Não\n3-Não sei responder\n ")
-            print('-'*100)
-            resposta3 = input("Na empresa em que você trabalha existem políticas para promover equilíbrio entre vida pessoal e trabalho?\n1-Sim\n2-Não\n3-Não sei responder\n ")
-            print('-'*100)
-            resposta4 = input("Você acha importante que as empresas adotem políticas/programas de bem-estar para cuidar da saúde\nmental dos seus funcionários?\n1-Sim\n2-Não\n3-Não sei responder\n ")
-            print('-'*100)
+            print('-' * 100)
+            print('\033[33mOlá! Está é uma pesquisa sobre equilíbrio entre vida pessoal, trabalho e saúde mental.')
+
+            try:
+                idade = int(input("\033[0;0mInforme sua idade (ou '00' para sair): "))
+                if idade == 0:  # Verifica se o usuário deseja sair da pesquisa
+                    break
+                elif idade < 0:  # Verifica se a idade é um valor válido
+                    print("Idade inválida. Por favor, digite um valor numérico positivo para a idade.")
+                    continue  # Reinicia o loop para solicitar a idade novamente em caso de valor inválido
+            except ValueError:  # Captura o erro caso o usuário não digite um número
+                print("Idade inválida. Por favor, digite um valor numérico para a idade.")
+                continue  # Reinicia o loop para solicitar a idade novamente em caso de valor inválido
+
+            print('-' * 100)
+
+            genero = input("Informe seu Gênero\n1 - Feminino\n2 - Masculino\n3 - Transgênero\n4 - Outro\n ")
+            print('-' * 100)
+
+            genero_opcao = {'1': 'Feminino', '2': 'Masculino', '3': 'Transgênero', '4': 'Outro'}.get(genero)  # Mapeia a escolha do gênero para a opção correspondente
+
+            resposta1 = self.obter_resposta("A sua empresa oferece suporte adequado à saúde mental dos funcionários?\n1 - Sim\n2 - Não\n3 - Não sei responder\n ")
+            resposta2 = self.obter_resposta("Você já passou por situações de sobrecarga no seu trabalho?\n1 - Sim\n2 - Não\n3 - Não sei responder\n ")
+            resposta3 = self.obter_resposta("Na empresa em que você trabalha, existem políticas para promover equilíbrio entre vida pessoal e trabalho?\n1 - Sim\n2 - Não\n3 - Não sei responder\n ")
+            resposta4 = self.obter_resposta("Você acha importante que as empresas adotem políticas/programas de bem-estar para cuidar da saúde mental dos seus funcionários?\n1 - Sim\n2 - Não\n3 - Não sei responder\n ")
+
+            print('-' * 100)
             print('\033[36mObrigado por responder nossa pesquisa!')
-            print('-'*100)
+            print('-' * 100)
 
-            data_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            # Dicionário para guardar em forma de valor as entradas pedidas ao usuário
-            respostas = { 
-                          'idade': idade,
-                          'genero': genero_opcao,
-                          'lista_respostas': [resposta1, resposta2, resposta3, resposta4],
-                          'data_hora': data_hora
-                          }
+            data_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Obtém a data e hora atual
 
-            self.dados_respostas.append(respostas)
-    
+            respostas = {
+                'idade': idade,
+                'genero': genero_opcao,
+                'lista_respostas': [resposta1, resposta2, resposta3, resposta4],
+                'data_hora': data_hora
+            }
+
+            self.dados_respostas.append(respostas)  # Adiciona as respostas à lista de dados de respostas
+
+    def obter_resposta(self, mensagem):
+        while True:
+            try:
+                resposta = int(input(mensagem))
+                if resposta not in [1, 2, 3]:  # Verifica se a resposta está dentro das opções válidas
+                    print("Opção inválida. Por favor, selecione uma opção válida.")
+                else:
+                    return resposta
+            except ValueError:  # Captura o erro caso o usuário não digite um número
+                print("Opção inválida. Por favor, selecione uma opção válida.")
+
     def salvar_csv(self):
         try:
             nome_arquivo = 'pesquisa_saude_trabalho'
 
-            
-            # Verifica se a extensão .csv está presente no nome do arquivo
-            if not nome_arquivo.endswith('.csv'):
+            if not nome_arquivo.endswith('.csv'):  # Verifica se o nome do arquivo termina com ".csv"
                 nome_arquivo += '.csv'
-            
-            with open(nome_arquivo, 'w', newline='') as arquivo_csv:
-                campos = ['idade', 'genero', 'resposta_1', 'resposta_2', 'resposta_3', 'resposta_4', 'data_hora_resposta']
-                writer = csv.DictWriter(arquivo_csv, fieldnames=campos)
-                writer.writeheader()
-                
+
+            with open(nome_arquivo, 'w', newline='') as arquivo_csv:  # Abre o arquivo CSV para escrita
+                campos = ['Idade', 'Gênero', 'Resposta_1', 'Resposta_2', 'Resposta_3', 'Resposta_4', 'data_hora_resposta']
+                escritor = csv.DictWriter(arquivo_csv, fieldnames=campos)
+                escritor.writeheader()  # Escreve o cabeçalho no arquivo CSV
+
                 for resposta in self.dados_respostas:
-                    writer.writerow({
+                    escritor.writerow({
                         'idade': resposta['idade'],
                         'genero': resposta['genero'],
                         'resposta_1': resposta['lista_respostas'][0],
@@ -85,16 +92,15 @@ class Pesquisa:
                         'resposta_3': resposta['lista_respostas'][2],
                         'resposta_4': resposta['lista_respostas'][3],
                         'data_hora_resposta': resposta['data_hora']
-                    })
-                
-                print('\033[32mOs dados foram salvos no arquivo CSV com sucesso!')
-        except IOError:
+                    })  # Escreve as respostas no arquivo CSV
+
+            print('\033[32mOs dados foram salvos no arquivo .CSV com sucesso!')
+        except IOError:  # Captura o erro caso ocorra um problema ao salvar o arquivo
             print('\033[41mErro ao salvar o arquivo. Verifique o nome e a permissão do diretório.')
-        except Exception as e:
+        except Exception as e:  # Captura erros inesperados
             print(f'\033[41mOcorreu um erro inesperado: {str(e)}')
 
-# Execução do programa
+# Cria uma instância da classe Pesquisa e executa o fluxo do programa
 pesquisa = Pesquisa()
 pesquisa.coletar_respostas()
 pesquisa.salvar_csv()
-
